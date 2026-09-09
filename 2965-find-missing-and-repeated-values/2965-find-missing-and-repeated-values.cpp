7626f99e3b718f870e60a5e51521a7fbd86d02c8
AtharvaPatil7967 <159103434+AtharvaPatil7967@users.noreply.github.com>
2026-09-09 16:29:14 +0530

    Time: 0 ms (100%), Space: 25.6 MB (86.17%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int total_elements = n * n;
+        long long n = grid.size();
+        long long N = n * n;
 
-        vector<int> hash(total_elements + 1, 0);
+        long long SN = (N * (N+1)) / 2;
+        long long S2N = (N * (N+1) * (2 * N + 1)) / 6;
 
-        for(int i=0; i<n; i++)
+        long long S=0, S2=0;
+
+        for(int i=0; i<n ;i++)
         {
             for(int j=0; j<n; j++)
             {
-                hash[grid[i][j]]++;
-            }
-        }
-        int repeating = -1;
-        int missing = -1;
-
-        for(int i=1; i<=total_elements; i++)
-        {
-
-            if(hash[i] == 2) repeating = i;
-            else if(hash[i] == 0) missing = i;
-
-            if(repeating != -1 && missing != -1)
-            {
-                break;
+                S += grid[i][j];
+                S2 += (long long)grid[i][j] * (long long) grid[i][j];
             }
         }
 
-        return {repeating, missing};
+        long long val1 = S - SN;
+        long long val2 = S2 - S2N;
+
+        val2 = val2 / val1;
+
+        long long x = (val1 + val2) / 2;
+        long long y = x - val1;
+
+        return {(int)x, (int)y};
     }
 };
