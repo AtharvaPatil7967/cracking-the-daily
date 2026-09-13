ece1e9e26a60673ac832db24fae826ce8acb1c00
AtharvaPatil7967 <159103434+AtharvaPatil7967@users.noreply.github.com>
2026-09-13 09:37:01 +0530

    Time: 0 ms (100%), Space: 18.1 MB (46.39%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -4,16 +4,51 @@ public:
 
         int n = nums.size();
 
-        for(int i=0; i<n; i++)
+        int low = 0;
+        int high = n - 1;
+
+        while(low <= high)
         {
-            if(nums[i] == target)
+            int mid = low + ((high - low) / 2);
+
+            if(nums[mid] == target)
             {
                 return true;
-                break;
+            }
+
+            if(nums[low] == nums[mid] && nums[mid] == nums[high])
+            {
+                low ++;
+                high --;
+                continue;
+            }
+
+
+
+            if(nums[low] <= nums[mid])
+            {
+                if(nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                } 
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            else
+            {
+                if(nums[mid] <= target && nums[high] >= target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
             }
         }
         return false;
-
         
     }
 };
