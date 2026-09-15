5b6317ce70536a9f62c6f84aeb0c4c3fde469baf
AtharvaPatil7967 <159103434+AtharvaPatil7967@users.noreply.github.com>
2026-09-15 10:34:52 +0530

    Time: 12 ms (14.53%), Space: 8.6 MB (14.27%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if(x == 0) return 0;
+        int ans = 1;
+
+        for(long long i=1; i<=x; i++)
+        {
+            if(i * i <= x)
+            {
+                ans = i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return ans;
+    }
+};
